424145f5cafea32f9285d1b4231e08fc2ab54686
esmartdie <esmartdie@gmail.com>
2023-10-12 16:39:13 +0200

    Challenge 40

---
+++ src/main/java/challenge_40/PascalTriangle.java
@@ -0,0 +1,25 @@
+package challenge_40;
+
+public class PascalTriangle {
+
+    public static void main(String[] args) {
+        int sideSize = 8;
+        drawEquilateralPascalTriangle(sideSize);
+    }
+
+    public static void drawEquilateralPascalTriangle(int sideSize) {
+        for (int i = 0; i < sideSize; i++) {
+
+            for (int j = 0; j < sideSize - i; j++) {
+                System.out.print(" ");
+            }
+
+            int current = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(current + " ");
+                current = current * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
